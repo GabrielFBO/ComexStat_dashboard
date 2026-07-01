23d00b8d59d67e087a54f15e003184bc0aabf153
GabrielFBO <gabriel.bispo98@hotmail.com>
2026-07-01 17:18:16 -0300

    Version 1.1: Data update

--- SQL/export_month.sql
+++ SQL/export_month.sql
@@ -1,6 +1,4 @@
 USE [comexstat]
 GO
 
-SELECT country_name FROM exportation GROUP BY country_name ORDER BY country_name 
-
 SELECT year, month, country_name, SUM(usd_value) AS total FROM exportation GROUP BY year, month, country_name ORDER BY CASE month WHEN 'Janeiro' THEN 1 WHEN 'Fevereiro' THEN 2 WHEN 'Março' THEN 3 WHEN 'Abril' THEN 4 WHEN 'Maio' THEN 5 WHEN 'Junho' THEN 6 WHEN 'Julho' THEN 7 WHEN 'Agosto' THEN 8 WHEN 'Setembro' THEN 9 WHEN 'Outubro' THEN 10 WHEN 'Novembro' THEN 11 WHEN 'Dezembro' THEN 12 END, year, country_name
--- SQL/import_month.sql
+++ SQL/import_month.sql
@@ -1,5 +1,4 @@
 USE [comexstat]
 GO
 
-
 SELECT year, month, country_name, SUM(usd_value) AS total FROM importation GROUP BY year, month, country_name ORDER BY CASE month WHEN 'Janeiro' THEN 1 WHEN 'Fevereiro' THEN 2 WHEN 'Março' THEN 3 WHEN 'Abril' THEN 4 WHEN 'Maio' THEN 5 WHEN 'Junho' THEN 6 WHEN 'Julho' THEN 7 WHEN 'Agosto' THEN 8 WHEN 'Setembro' THEN 9 WHEN 'Outubro' THEN 10 WHEN 'Novembro' THEN 11 WHEN 'Dezembro' THEN 12 END, year, country_name
